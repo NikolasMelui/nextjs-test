b25fd36135a6b93b6b2547a1dec596e3cad09ffa
nikolasmelui <slak@samaradom.ru>
2020-02-05 22:28:58 +0300

    Create dynamic client and serverside render with data fetching on Posts page

--- pages/posts.js
+++ pages/posts.js
@@ -1,23 +1,30 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
 const PostLink = props => (
   <li>
     <Link href="/posts/[id]" as={`/posts/${props.id}`}>
-      <a>{props.id}</a>
+      <a>{props.name}</a>
     </Link>
   </li>
 );
 
-const Posts = () => (
+const Posts = props => (
   <Layout>
-    <p>Blog</p>
+    <h1>Posts</h1>
     <ul>
-      <PostLink id="First post" />
-      <PostLink id="Second post" />
-      <PostLink id="Third post" />
+      {props.posts.map(({ id, name }) => (
+        <PostLink key={id} id={id} name={name} />
+      ))}
     </ul>
   </Layout>
 );
+
+Posts.getInitialProps = async () => ({
+  posts: (
+    await (await fetch("https://api.tvmaze.com/search/shows?q=batman")).json()
+  ).map(entry => entry.show)
+});
 
 export default Posts;
