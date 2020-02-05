8f70a606f03621344ad349846dfc18e145d2ee6c
nikolasmelui <slak@samaradom.ru>
2020-02-05 21:06:20 +0300

    Fix main index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
 import Layout from "../components/MyLayout";
-import Link from "next/link";
+import Header from "../components/Header";
 
-const PostLink = props => (
-  <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+const Main = () => (
+  <div>
+    <Layout>
+      <p>Main page</p>
+    </Layout>
+  </div>
 );
 
-const Blog = () => (
-  <Layout>
-    <p>Blog</p>
-    <ul>
-      <PostLink title="First post" />
-      <PostLink title="Second post" />
-      <PostLink title="Third post" />
-    </ul>
-  </Layout>
-);
-
-export default Blog;
+export default Main;
