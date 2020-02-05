08188d31cf9a1d12c326a85c2819a3041cd1de38
nikolasmelui <slak@samaradom.ru>
2020-02-05 20:47:47 +0300

    Create routes for dynamic posts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Layout from "../components/MyLayout";
+import Link from "next/link";
 
-const Index = () => (
-  <div>
-    <Layout>
-      <p>Main page</p>
-    </Layout>
-  </div>
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+const Blog = () => (
+  <Layout>
+    <p>Blog</p>
+    <ul>
+      <PostLink title="First post" />
+      <PostLink title="Second post" />
+      <PostLink title="Third post" />
+    </ul>
+  </Layout>
+);
+
+export default Blog;
