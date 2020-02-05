085d4be7d2054b884dcd09d28058dd06c6777c8a
nikolasmelui <slak@samaradom.ru>
2020-02-05 22:29:05 +0300

    Create dynamic client and serverside render with data fetching on Posts page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,11 +1,18 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/MyLayout";
 
-const Post = () => (
+const Post = props => (
   <Layout>
-    <h1>{useRouter().query.id}</h1>
-    <p>This is the {useRouter().query.id} post content.</p>
+    <h1>{props.post.name}</h1>
+    <p>{props.post.summary.replace(/<[/]?[pb]>/g, "")}</p>
+    {props.post.image ? <img src={props.post.image.medium} /> : null}
   </Layout>
 );
+
+Post.getInitialProps = async context => ({
+  post: await (
+    await fetch(`https://api.tvmaze.com/shows/${context.query.id}`)
+  ).json()
+});
 
 export default Post;
